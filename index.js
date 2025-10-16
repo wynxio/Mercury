d41ffeeac54babe15da3be29f85f5530111bb635
wynxio <wynxiotech@gmail.com>
2025-10-16 11:13:29 +0530

    variables

---
+++ index.js
@@ -0,0 +1,19 @@
+// Variable
+// var is  a keyword for creating variable
+// name is identifier
+//sabeer is the value
+var name = "sabeer"
+var Name = "sabeer1"
+var fullName = 'sabeermn'
+console.log(name)
+console.log(Name)
+
+// Standared - Camel Case fullName
+
+// Identifier Guidelines / Naming Rules
+//contain only letter, digit, $ , _  example wyn555$_ invalid example name@
+//should not start with digit valid name1 invalid 1name
+// should not use reserved keywords example keywords var,let public 
+//case sensitive name and Name are different
+//can be of any length
+
---
+++ index2.js
@@ -0,0 +1,33 @@
+// 3 ways to create variables
+//var let const
+
+
+// const name='wynxio222wwww'
+// console.log(name)
+
+
+
+// const pi = 3.14
+ 
+// console.log(age)
+
+// Const
+//cannot redeclare
+//cannot re assign the value
+
+//let
+// let name ='sabeer'
+// name ='sabeermn'
+// console.log(name)
+
+// let
+//cannot redeclare
+//can re assign the  value
+
+//var
+var name ='sabeer'
+name ='sabeermn'
+console.log(name)
+
+//can redeclare
+//can re assign the  value
